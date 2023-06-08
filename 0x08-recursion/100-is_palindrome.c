861fa259b892af583fff10096d6f525fe734dd95
Christabelchioma <okpetahchioma@gmail.com>
2023-06-07 21:13:28 -0700

    RECURSION

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,13 +8,13 @@
 
 int is_palindrome(char *s)
 {
-        int index = 0;
-        int len = find_strlen(s);
+	int index = 0;
+	int len = find_strlen(s);
 
-        if (!(*s))
-                return (1);
+	if (!(*s))
+		return (1);
 
-        return (check_palindrome(s, len, index));
+	return (check_palindrome(s, len, index));
 }
 
 /**
@@ -25,15 +25,15 @@ int is_palindrome(char *s)
  */
 int find_strlen(char *s)
 {
-        int len = 0;
+	int len = 0;
 
-        if (*(s + len))
-        {
-                len++;
-                len += find_strlen(s + len);
-        }
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
 
-        return (len);
+	return (len);
 }
 
 /**
@@ -47,11 +47,11 @@ int find_strlen(char *s)
  */
 int check_palindrome(char *s, int len, int index)
 {
-        if (s[index] == s[len / 2])
-                return (1);
+	if (s[index] == s[len / 2])
+		return (1);
 
-        if (s[index] == s[len - index - 1])
-                return (check_palindrome(s, len, index + 1));
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
 
-        return (0);
+	return (0);
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,13 +9,13 @@
 
 int factorial(int n)
 {
-        int next;
+	int next;
 
-        if (n == 0)
-                return (1);
-        else if (n < 0)
-        return (-1);
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
 
-        next = factorial(n - 1);
-        return (n * next);
+	next = factorial(n - 1);
+	return (n * next);
 }
